af2c8de0a50a6bec5ff0e9b541fa23fcd91324dc
Fizur <fizurmia10@gmail.com>
2024-09-29 16:32:25 +0600

    2 screen done

---
+++ lib/const/app_color.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const gradient1 = Color(0xffeef2ff);
+  static const gradient2 = Color(0xffe0e7ff);
+  static const onboardingLightYellow = Color(0xffFFFAEB);
+  static const onboardingDarkYellow = Color(0xffFEF0C7);
+  static const primaryColor = Color(0xff4F46E5);
+  static const indigo200 = Color(0xff9fa8da);
+  static const indigo = Color(0xff4F46E5);
+  static const indigo50 = Color(0xffEEF2FF);
+  static const yellow300 = Color(0xffFDE047);
+  static const textcolor3 = Color(0xff475569);
+  static const labelColor = Color(0xff64748B);
+  static const textColor2 = Color(0xff94A3B8);
+  static const borderColor = Color(0xffE2E8F0);
+  static const greenColor = Color(0xff12B76A);
+  static const redColor = Color(0xffF04438);
+
+  static const textColor1 = Color(0xff1E293B);
+  static const slate900 = Color(0xff0f172a);
+  static const white = Color(0xffffffff);
+  static const whiteTextColor = Color(0xffF8FAFC);
+  static const black = Color(0xff000000);
+}
